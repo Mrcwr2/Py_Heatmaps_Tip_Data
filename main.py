04a665d9a9014af773bf51f21b727dbbd0f3a93d
Charles Robinson <40834093+Mrcwr2@users.noreply.github.com>
2021-09-26 00:24:44 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+with open("tips.csv", "r") as csvfile:
+  tips = pd.read_csv(csvfile, delimiter=",")
+tips_pivoted = tips.pivot_table(
+values="tip",
+index =["size"],
+columns=["time"]
+)
+fig = sns.heatmap(tips_pivoted, annot=True,cmap="coolwarm")
+
+fig.set_ylim(0,6)
+
+plt.xlabel("Time")
+plt.ylabel("Tip amount") 
+plt.title("Tips Heatmap")
+
+plt.savefig("Tips_Heatmap.png")
